d00a8bfa16a405dd3a367c4264cad63210d5e5ec
Kieran Reid <kieranreid2612@gmail.com>
2026-04-18 16:42:57 +0100

    Center login

--- app/routes/sign-in.tsx
+++ app/routes/sign-in.tsx
@@ -3,7 +3,9 @@ import { SignIn}  from "@clerk/react-router";
 export default function SignInPage() {
   return (
     <div className="flex items-center w-screen justify-center h-screen">
-      <SignIn />
+      <div className="ml-40">
+        <SignIn />
+      </div>
     </div>
   );
 }
